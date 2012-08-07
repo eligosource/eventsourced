d914d7e31031d3bb88139623acc2114cb6140dc5
Martin Krasser <krasserm@googlemail.com>
2012-08-07 15:48:05 +0200

    Documentation section 'Diving deeper' plus example code

--- src/test/scala/org/eligosource/eventsourced/example/OrderExample.scala
+++ src/test/scala/org/eligosource/eventsourced/example/OrderExample.scala
@@ -1,6 +1,9 @@
 package org.eligosource.eventsourced.example
 
 import akka.actor._
+import akka.pattern.ask
+import akka.util.duration._
+import akka.util.Timeout
 
 import org.eligosource.eventsourced.core._
 
@@ -70,4 +73,63 @@ object OrderExample1 extends App {
   }
 }
 
+object OrderExample2 extends App {
+  implicit val system = ActorSystem("example")
+  implicit val timeout = Timeout(5 seconds)
+
+  // create a journal
+  val journalDir = new java.io.File("target/example")
+  val journal = system.actorOf(Props(new Journal(journalDir)))
+
+  // create a destination for output events
+  val destination = system.actorOf(Props[Destination])
+
+  // create and configure an event-sourcing component
+  // with event processor and a named output channel
+  val orderComponent = Component(1, journal)
+    .addDefaultOutputChannelToActor("dest", destination)
+    .setProcessor(outputChannels => system.actorOf(Props(new Processor(outputChannels))))
+
+  // recover processor state from journaled events
+  orderComponent.init()
+
+  // send some events
+  orderComponent.inputChannel ! Message(OrderSubmitted(Order("foo")))
+  orderComponent.inputChannel ! Message(OrderSubmitted(Order("bar")))
+
+  // and expect a reply
+  orderComponent.inputProducer ? OrderSubmitted(Order("baz")) onSuccess {
+    case order: Order => println("received order %s" format order)
+  }
+
+  // wait for output events to arrive (graceful shutdown coming soon)
+  Thread.sleep(1000)
+
+  // then shutdown
+  system.shutdown()
+
+  // event-sourced processor
+  class Processor(outputChannels: Map[String, ActorRef]) extends Actor {
+    var orders = Map.empty[Int, Order] // processor state
+
+    def receive = {
+      case msg: Message => msg.event match {
+        case OrderSubmitted(order) => {
+          val id = orders.size
+          val upd = order.copy(id = id)
+          orders = orders + (id -> upd)
+          msg.sender.foreach(_ ! upd)
+          outputChannels("dest") ! msg.copy(event = OrderAccepted(upd))
+        }
+      }
+    }
+  }
+
+  // output event destination
+  class Destination extends Actor {
+    def receive = {
+      case msg: Message => { println("received event %s" format msg.event); sender ! Ack }
+    }
+  }
+}
 
