4d42a219d4b04f303586c2055994cf47bb20450f
Martin Krasser <krasserm@googlemail.com>
2012-11-20 18:02:54 +0100

    Add protobuf dependency

--- project/Build.scala
+++ project/Build.scala
@@ -29,7 +29,7 @@ object Resolvers {
 object Dependencies {
   import Dependency._
 
-  val core = Seq(akkaActor, akkaCluster, commonsIo, journalIo, levelDbJni, scalaTest, scalaActors)
+  val core = Seq(akkaActor, akkaCluster, commonsIo, journalIo, levelDbJni, protobuf, scalaTest, scalaActors)
 }
 
 object Version {
@@ -44,10 +44,11 @@ object Dependency {
   //  Compile
   // -----------------------------------------------
 
-  val akkaActor   = "com.typesafe.akka"         %% "akka-actor" % Akka % "compile"
-  val commonsIo   = "commons-io"                %  "commons-io"        % "2.3"   % "compile"
-  val journalIo   = "journalio"                 %  "journalio"         % "1.2"   % "compile"
-  val levelDbJni  = "org.fusesource.leveldbjni" %  "leveldbjni-all"    % "1.4.1" % "compile"
+  val protobuf    = "com.google.protobuf"       %  "protobuf-java"  % "2.4.1" % "compile"
+  val akkaActor   = "com.typesafe.akka"         %% "akka-actor"     % Akka    % "compile"
+  val commonsIo   = "commons-io"                %  "commons-io"     % "2.3"   % "compile"
+  val journalIo   = "journalio"                 %  "journalio"      % "1.2"   % "compile"
+  val levelDbJni  = "org.fusesource.leveldbjni" %  "leveldbjni-all" % "1.4.1" % "compile"
 
   // -----------------------------------------------
   //  Test
