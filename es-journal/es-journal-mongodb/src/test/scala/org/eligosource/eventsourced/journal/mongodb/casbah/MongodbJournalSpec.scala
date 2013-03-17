8fce780db08b4abeb136d552da651c5e21e80bb3
duncan.devore@gmail.com <duncan.devore@gmail.com>
2013-03-17 13:22:49 -0400

    Added testing support for core-test.

--- es-journal/es-journal-mongodb/src/test/scala/org/eligosource/eventsourced/journal/mongodb/casbah/MongodbJournalSpec.scala
+++ es-journal/es-journal-mongodb/src/test/scala/org/eligosource/eventsourced/journal/mongodb/casbah/MongodbJournalSpec.scala
@@ -1,56 +1,35 @@
 /*
- * Copyright 2012-2013 Eligotech BV.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2012-2013 Eligotech BV.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package org.eligosource.eventsourced.journal.mongodb.casbah
 
 import org.scalatest.BeforeAndAfterEach
 import com.mongodb.casbah.Imports._
 import org.eligosource.eventsourced.journal.common.JournalSpec
-import de.flapdoodle.embed.mongo.config.MongodConfig
-import de.flapdoodle.embed.mongo.distribution._
-import de.flapdoodle.embed.process.runtime.Network
-import de.flapdoodle.embed.mongo.{MongodProcess, MongodExecutable, MongodStarter}
 
-class MongodbJournalSpec extends JournalSpec with BeforeAndAfterEach {
+class MongodbJournalSpec extends JournalSpec with MongodbSpecSupport with BeforeAndAfterEach {
 
-  val embedMongoDBName = "casbah"
-  val embedMongoDBColl = "journal"
-  val embedMongoDBConnPort = 12345
-  val embedMongoDBVer = Version.V2_2_1
-
-  var runtime: MongodStarter = null
-  var mongodExe: MongodExecutable = null
-  var mongod: MongodProcess = null
-  var mongoConn: MongoConnection = null
+  // Since multiple embedded instances will run, each one must have a different port.
+  override def mongoDBConnPort = 12345
 
   def journalProps = MongodbJournalProps(journalColl)
 
-  def journalColl: MongoCollection = mongoConn(embedMongoDBName)(embedMongoDBColl)
-
-  override def beforeEach() {
-    runtime = MongodStarter.getDefaultInstance
-    mongodExe = runtime.prepare(new MongodConfig(embedMongoDBVer, embedMongoDBConnPort, Network.localhostIsIPv6()))
-    mongod = mongodExe.start()
-    mongoConn = MongoConnection(Network.getLocalHost.getCanonicalHostName, embedMongoDBConnPort)
-  }
+  def journalColl: MongoCollection = mongoConn(mongoDBName)(mongoDBColl)
 
   override def afterEach() {
     journalColl.dropCollection()
-    mongoConn.dropDatabase(embedMongoDBName)
-    mongoConn.close()
-    mongod.stop()
-    mongodExe.stop()
+    mongoConn.dropDatabase(mongoDBName)
   }
 }
